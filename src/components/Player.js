d6f4357267a0c679c4093897f505763543641b35
flamerged <mersad.ajanovic@googlemail.com>
2020-03-11 10:56:59 +0100

    Initial commit

---
+++ src/components/Player.js
@@ -0,0 +1,37 @@
+import React, { PureComponent } from 'react';
+import { Consumer } from './Context';
+import PropTypes from 'prop-types';
+import Counter from './Counter';
+
+class Player extends PureComponent {
+    static propTypes = {
+        index: PropTypes.number.isRequired
+    };
+
+    render() {
+        const { index } = this.props;
+        return (
+            <Consumer>
+                {({ actions, players }) => (
+                    <div className="player">
+                        <span className="player-name">
+                            <button
+                                className="remove-player"
+                                onClick={() =>
+                                    actions.removePlayer(players[index].id)
+                                }
+                            >
+                                ✖
+                            </button>
+                            {players[index].name}
+                        </span>
+
+                        <Counter index={index} />
+                    </div>
+                )}
+            </Consumer>
+        );
+    }
+}
+
+export default Player;
